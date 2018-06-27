dbd07e7e870ec852eb26e5e2cb57c29692442774
CzarcasticJack <joshua.weaver@colorado.edu>
2018-06-26 21:23:03 -0600

    Refactors, fix numMonsters to go by wave, create new TextViews for each monster, added scrollview, UI touch-ups, updated minSDKVersion to 17 for id generation

--- app/src/main/java/com/josh/kingsarmorycompanion/MonstersActivity.java
+++ app/src/main/java/com/josh/kingsarmorycompanion/MonstersActivity.java
@@ -1,17 +1,20 @@
 package com.josh.kingsarmorycompanion;
 
-import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
-import java.util.ArrayList;
+
 import java.util.Random;
 
 public class MonstersActivity extends AppCompatActivity {
 
     public String message;
 
-    int chart[][] = new int[][]{
+    static final int CHART[][] = new int[][]{
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3},
             {0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4},
@@ -21,15 +24,12 @@ public class MonstersActivity extends AppCompatActivity {
             {0, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5},
             {0, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5}};
 
-    int[] monsterArray = {0, 3, 5, 7, 9, 11, 13, 15};
+    static final int[] NUM_MONSTERS = {0, 3, 5, 7, 9, 11, 13, 15};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monsters);
-
-        Intent intent = getIntent();
-        message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
     }
 
     protected void onStart(){
@@ -38,31 +38,35 @@ public class MonstersActivity extends AppCompatActivity {
 
         int wave = MainActivity.wave;
         int numPlayers = MainActivity.numPlayers;
-        String side = "";
-        String result = "";
-
+        String side;
+        Random r = new Random();
+        int roll;
+        ConstraintLayout main = (ConstraintLayout) findViewById(R.id.monsterLayout);
+        ConstraintSet constraintSet = new ConstraintSet();
         int numMonsters = 0;
+        int lastViewId = -1;
+
         switch (numPlayers) {
             case 1:
-                numMonsters = monsterArray[numPlayers] - 1;
+                numMonsters = NUM_MONSTERS[wave] - 1;
                 break;
             case 2:
-                numMonsters = monsterArray[numPlayers];
+                numMonsters = NUM_MONSTERS[wave];
                 break;
             case 3:
-                numMonsters = monsterArray[numPlayers] + 1;
+                numMonsters = NUM_MONSTERS[wave] + 1;
                 break;
             case 4:
-                numMonsters = monsterArray[numPlayers] + 2;
+                numMonsters = NUM_MONSTERS[wave] + 2;
                 break;
             case 5:
-                numMonsters = monsterArray[numPlayers] + 3;
+                numMonsters = NUM_MONSTERS[wave] + 3;
                 break;
             case 6:
-                numMonsters = monsterArray[numPlayers] + 3;
+                numMonsters = NUM_MONSTERS[wave] + 3;
                 break;
             case 7:
-                numMonsters = monsterArray[numPlayers] + 3;
+                numMonsters = NUM_MONSTERS[wave] + 3;
                 break;
             default:
                 //error
@@ -70,49 +74,52 @@ public class MonstersActivity extends AppCompatActivity {
         }
 
         for(int i=0; i<numMonsters; i++ ){
-            Random r = new Random();
-            int m1 = r.nextInt(13-1)+1;
+            roll = r.nextInt(13-1)+1;
 
-            if(m1%2==1)
-                side = "ODD";
-            else
-                side = "EVEN";
+            side = (roll%2 == 1) ? "ODD" : "EVEN";
 
             switch (numPlayers){
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
                 case 4:
-                    m1+= 1;
+                    roll+= 1;
                     break;
                 case 5:
-                    m1+= 1;
+                    roll+= 1;
                     break;
                 case 6:
-                    m1+= 2;
+                    roll+= 2;
                     break;
                 case 7:
-                    m1+= 3;
+                    roll+= 3;
                     break;
                 default:
-                    //error
                     break;
             }
 
-            if(m1 > 12)
-                m1 = 12;
+            if (roll > 12) {
+                roll = 12;
+            }
 
 
-            result+= "Level " + chart[wave][m1] + " monster on " + side + " side. (" + m1 + ")\n";
+
+            TextView resultText = new TextView(this);
+            resultText.setId(View.generateViewId());
+            resultText.setText("Level " + CHART[wave][roll] + " monster on " + side + " side. (" + roll + ")");
+            resultText.setGravity(Gravity.CENTER);
+            main.addView(resultText);
+            constraintSet.clone(main);
+
+            if (i == 0) {
+                constraintSet.connect(resultText.getId(), ConstraintSet.TOP, main.getId(), ConstraintSet.BOTTOM, 0);
+            } else {
+                constraintSet.connect(resultText.getId(), ConstraintSet.TOP, lastViewId, ConstraintSet.BOTTOM, 0);
+            }
+            constraintSet.connect(resultText.getId(), ConstraintSet.RIGHT, main.getId(), ConstraintSet.RIGHT, 0);
+            constraintSet.connect(resultText.getId(), ConstraintSet.LEFT, main.getId(), ConstraintSet.LEFT, 0);
+            constraintSet.constrainHeight(resultText.getId(),150);
+            lastViewId = resultText.getId();
+            constraintSet.applyTo(main);
         }
-
-        TextView resultText = (TextView) findViewById(R.id.result_text);
-        resultText.setText(result);
-
+        constraintSet.clone(main);
     }
-
 }
 
